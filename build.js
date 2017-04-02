74f50f4c99fc6ed0cc4a910bebb5e682b3d9448a
nukos <nukos.kitchen@gmail.com>
2017-04-02 23:37:16 +0900

    first commit

---
+++ build.js
@@ -0,0 +1,16 @@
+var sass = require('node-sass');
+var fs   = require('fs');
+
+sass.render({
+  file: 'style.scss',
+  outFile: 'style.css',
+}, function(error, result) { // node-style callback from v3.0.0 onwards
+  if(!error){
+    // No errors during the compilation, write this result on the disk
+    fs.writeFile('style.css', result.css, function(err){
+      if(!err){
+        //file written on disk
+      }
+    });
+  }
+});
