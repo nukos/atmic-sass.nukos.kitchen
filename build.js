c618644ce7fc7dc0e125f01a6021602051cb0cc5
Ryuichi Nonaka <nukos.kitchen@gmail.com>
2017-05-12 19:14:42 +0900

    wip

--- build.js
+++ build.js
@@ -2,7 +2,7 @@ var sass = require('node-sass');
 var fs   = require('fs');
 
 sass.render({
-  file: 'style.scss',
+  file: 'core/style.scss',
   outFile: 'style.css',
 }, function(error, result) { // node-style callback from v3.0.0 onwards
   if(!error){
